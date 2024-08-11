fe373f0800f97913e9f5a292688cab3e1df84537
Tanmoy Chanda <tanmoy.chanda@outlook.com>
2024-08-11 23:36:31 +0530

    update

--- script.js
+++ script.js
@@ -22,3 +22,58 @@ document.addEventListener('DOMContentLoaded', function() {
 
     scrollBrands();
 });
+var swiper = new Swiper(".mySwiperslider", {
+    spaceBetween: 50,
+    slidesPerView: 1,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay:{
+        delay:3000
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    history: {
+      key: "slide",
+    },
+  });
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 16,
+    autoplay:{
+        delay:3000
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  //Check if device is online or offline
+  window.addEventListener('load', function() {
+    // Function to check the online/offline status
+    function checkConnection() {
+        if (!navigator.onLine) {
+            alert("No internet connection");
+            document.getElementById('container').style.display = 'none';
+            document.getElementById('no-connection').style.display = 'block';
+        }
+    }
+
+    // Check the connection status when the page loads
+    checkConnection();
+
+    // Listen for changes in the online/offline status
+    window.addEventListener('offline', function() {
+        alert("No internet connection");
+        document.getElementById('container').style.display = 'none';
+        document.getElementById('no-connection').style.display = 'block';
+    });
+
+    window.addEventListener('online', function() {
+        alert("Internet connection restored");
+        document.getElementById('container').style.display = 'block';
+        document.getElementById('no-connection').style.display = 'none';
+    });
+});
